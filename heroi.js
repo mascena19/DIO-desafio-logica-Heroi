21dac6f7a0823fd0749ac48a4f4ec073252e9638
Rodrigo Mascena - Unimed Goiania/TI <rodrigo.mascena@unimedgoiania.coop.br>
2023-12-12 11:46:03 -0300

    Projeto desafio DIO Heroi

---
+++ heroi.js
@@ -0,0 +1,36 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+  
+    atacar() {
+      let ataque;
+      switch (this.tipo) {
+        case "mago":
+          ataque = "usou magia";
+          break;
+        case "guerreiro":
+          ataque = "usou espada";
+          break;
+        case "monge":
+          ataque = "usou artes marciais";
+          break;
+        case "ninja":
+          ataque = "usou shuriken";
+          break;
+        default:
+          ataque = "usou um ataque indefinido";
+      }
+  
+      console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+    }
+  }
+  
+  // Exemplo de uso
+  const meuHeroi = new Heroi("Lancelot", 30, "guerreiro");
+  meuHeroi.atacar();
+  const meuHeroi2 = new Heroi("Rodrigo ", 30, "mago");
+  meuHeroi2.atacar();
+  
